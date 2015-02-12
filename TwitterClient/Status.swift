005347e3258bfdddc58fc59bd7edf09b310f2cd9
sujang <sujang.kang@gmail.com>
2015-02-13 07:47:19 +0900

    追加漏れ

---
+++ TwitterClient/Status.swift
@@ -0,0 +1,19 @@
+//
+//  Status.swift
+//  TwitterClient
+//
+//  Created by 姜 修章 on 2015/01/08.
+//  Copyright (c) 2015年 姜 修章. All rights reserved.
+//
+
+import UIKit
+
+class Status {
+    var text = ""
+    var name = ""
+    var iconURL = ""
+    var icon: UIImage?
+    
+    init() {
+    }
+}
